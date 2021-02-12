1ad4a3d9a5b68023692f08670eddc58772b1ff2c
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2021-02-12 15:17:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { CardList } from './components/card-list/card-list';
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        console.log(response)
+        setData(response.data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <CardList name="Ali">
+        {data.map(item => (
+          <h1 key={item.id}>{item.name}</h1>
+        ))}
+        </CardList>
     </div>
   );
 }
